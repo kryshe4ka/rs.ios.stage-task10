7debca1b5b85188fd27c1716ee3aab97821aa87e
kryshe4ka <kryshe4ka@gmail.com>
2021-08-25 16:36:51 +0300

    feat: start new project

---
+++ RSSchool_T10_GameCounter/RSSchool_T10_GameCounter/System/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  RSSchool_T10_GameCounter
+//
+//  Created by Liza Kryshkovskaya on 25.08.21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
